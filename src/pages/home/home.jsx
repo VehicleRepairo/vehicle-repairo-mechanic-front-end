bcec43043c725c097f786d40155dd8afac243c75
devindhi <136802380+devindhi@users.noreply.github.com>
2024-03-09 18:54:56 +0530

    Updated

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,28 +3,25 @@ import "./home.css";
 
 const HomePage = () => {
   return (
-    <div className="home-container">
-      <div className="image-container">
-        <img src="/Web_mech.jpg" alt="webMech" />
+    
+      <div className="home-container">
+        <div className="image-container">
+          <img src="/Web_mech.jpg" alt="webMech" style={{ width: '800px', height: '500px' }} />
+        </div>
+        <div className="content-container">
+          <p>
+            <strong>Welcome to Vehicle Repairo</strong>
+          </p>
+          <p> 
+            Join our platform to showcase your expertise,<br />
+            accept appointments, and receive feedback <br />
+            from vehicle owners. Be part of a community<br />
+            committed to delivering exceptional automotive service.
+          </p>
+          <br />
+          <p><i><h3>Let's redefine car care together!</h3></i></p>
+        </div>
       </div>
-      <div className="content-container">
-        <p>
-          <strong>Welcome to Vehicle Repairo</strong>
-        </p>
-        <p>
-          Join our platform to showcase your expertise,
-          accept appointments, and receive feedback
-          from vehicle owners. Be part of a community
-          committed to delivering exceptional automotive service.
-        </p>
-        <br />
-        <p>
-          <i>
-            <h3>Let's redefine car care together!</h3>
-          </i>
-        </p>
-      </div>
-    </div>
   );
 };
 
